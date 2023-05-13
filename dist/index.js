22491e065123703e0f3ebd0646a83412261e759b
Basil Suter <git@nadar.io>
2023-05-13 13:22:08 +0000

    reactive props

--- dist/index.js
+++ dist/index.js
@@ -55,9 +55,12 @@ var script = /*#__PURE__*/Object.assign(__default__, {
     default: false
   }
 },
-  setup(__props) {
+  emits: ['update:page'],
+  setup(__props, { emit: emits }) {
 
 const props = __props;
+
+
 
 
 
@@ -97,7 +100,8 @@ if (process.client && parentWindow(window)) {
 
     const message = event.data;
     if (message.action === 'pageRefresh') {
-      props.page = message.data;
+      emits('update:page', message.data);
+
       /*
       const itemIndex = props.page.json.findIndex(item => item.uid === data.uid)
       props.page.json[itemIndex] = {
